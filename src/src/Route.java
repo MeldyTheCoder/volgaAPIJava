33f61c88a84a10e2c7b230c9ae89b8c896ac27f1
Meldy <53063665+MeldyTheCoder@users.noreply.github.com>
2023-02-18 14:09:51 +0300

    Add files via upload

---
+++ src/src/Route.java
@@ -0,0 +1,7 @@
+public class Route {
+    public RouteInfo route;
+    public int route_id;
+    public Station station;
+    public int station_id;
+    public String vehicle_arrival_time;
+}
---
+++ src/src/RouteInfo.java
@@ -0,0 +1,14 @@
+public class RouteInfo {
+    public int agent_id;
+    public String colour_hex;
+    public String comment;
+    public String end_station;
+    public boolean is_circular;
+    public int location_id;
+    public String name;
+    public int route_id;
+    public String start_end_stations;
+    public String title;
+    public int vehicle_type_id;
+
+}
